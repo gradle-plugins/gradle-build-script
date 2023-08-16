933351936cc6b408b7a2039aa6859364f1d29c70
Daniel Lacasse <daniel@nokee.dev>
2023-08-16 14:59:43 -0400

    Ensure Groovy DSL literal are parsed into Gradle AST when outputing Kotlin
    Signed-off-by: Daniel Lacasse <daniel@nokee.dev>

--- src/main/java/dev/gradleplugins/buildscript/syntax/KotlinCompatibility.java
+++ src/main/java/dev/gradleplugins/buildscript/syntax/KotlinCompatibility.java
@@ -1,5 +1,6 @@
 package dev.gradleplugins.buildscript.syntax;
 
+import dev.gradleplugins.buildscript.syntax.normalizer.GroovyDslTransformer;
 import dev.gradleplugins.buildscript.syntax.normalizer.KotlinCastingTransformer;
 import dev.gradleplugins.buildscript.syntax.normalizer.KotlinPropertyAccessTransformer;
 import dev.gradleplugins.buildscript.syntax.normalizer.KotlinVariableTransformer;
@@ -10,6 +11,7 @@ import java.util.Arrays;
 public class KotlinCompatibility extends ASTNormalizer {
     public KotlinCompatibility() {
         super(Arrays.asList(
+                new GroovyDslTransformer(),
                 new MethodCallToGradleBlockTransformer(),
                 new KotlinPropertyAccessTransformer(),
                 new KotlinVariableTransformer(),
